7eee25e6aa41b570d212822bbf19ab0308212598
Alexandr.Danilov <>
2020-09-18 19:40:32 +0600

    init

--- src/main/kotlin/Animal.kt
+++ src/main/kotlin/Animal.kt
@@ -1,5 +1,5 @@
 enum class Sex { Male, Female, Other }
-enum class Disease
+enum class DeathCause {War, Disease, FromOldAge }
 
 abstract open class Baba {
     init {
@@ -12,24 +12,43 @@ abstract class Human() : Baba() {
         println("Human is born")
     }
 
+    abstract val name: String
+    abstract val age:Int
+    abstract var sex: Sex?
+
+    abstract fun getDetails()
+
+    abstract fun die(cause: DeathCause)
     open fun die() {
         println("Human is gone")
     }
 }
 
-class Man(val name: String, val age: Int) : Human() {
-    var sex: Sex? = null
-    fun getDetails(){
-        if (sex==null){println("This is a man, he's name is $name, he's age is $age and he is androgen")}
-        when (sex){
+class Man(override val name: String,override val age: Int) : Human() {
+    override var sex: Sex? = null
+    override fun getDetails() {
+        if (sex == null) {
+            println("This is a man, he's name is $name, he's age is $age and he is androgen")
+        }
+        when (sex) {
             Sex.Male -> println("This is a man, he's name is $name, he's age is $age and he is male")
             Sex.Female -> println("This is a man, he's name is $name, he's age is $age and he is transgender")
 
         }
     }
 
-    constructor(_name: String, _age: Int, _sex: Sex) : this(_name,_age) {
-        sex=_sex
+    override fun die(cause: DeathCause) {
+        super.die()
+        println("$name has died in age of $age years because of ${cause.name}")
+    }
+
+    fun goToWar(){
+        println("Doing some pif-paf")
+        this.die(DeathCause.War)
+    }
+
+    constructor(_name: String, _age: Int, _sex: Sex) : this(_name, _age) {
+        sex = _sex
 
     }
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,14 @@
-
-fun main(){
-   // println("Hello world")
+fun main() {
+    // println("Hello world")
     //Test(1).test()
-   // Test("test").test()
-    Man("Ваня",13).getDetails()
-    Man("Вивальди", 56,Sex.Female).getDetails()
-    Man("Колян", 16,Sex.Male).getDetails()
+    // Test("test").test()
+    val a = Man("Ваня", 13)
+    a.getDetails()
+    val b = Man("Вивальди", 56, Sex.Female)
+    b.getDetails()
+    b.goToWar()
+    val c = Man("Колян", 16, Sex.Male)
+    c.getDetails()
+    c.die()
+
 }
